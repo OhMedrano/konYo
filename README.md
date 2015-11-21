f6064b91939535b8e8c3ced3f8914de5cb1addb6
OhMedrano <Omedrano564@gmail.com>
2015-11-21 11:47:32 -0500

    Checking changes on digital ocean

---
+++ README.md
@@ -0,0 +1,224 @@
+8/6 	
+		
+
+		...I tend to use this README to help me troubleshoot. It's 
+		a version of the rubber ducky method, except I won't be 
+		talking to myself outloud. Also, figuring that some of my
+		friends might find this repo, I'll detail each part of it;
+		so you can follow along with the code. 
+
+
+		So I shall be addressing this as if I'm talking to you. 
+
+		Yes. 
+
+		You. 
+
+		Lets' continue. 
+
+
+		Ok so, the angular app is coming along. I had to
+		use Yeoman for a scaffolding. For some reason,
+		they never bother updating their tutorials since 
+		a lot of the controller functions weren't working. 
+		After checking into the problem, I find that the 
+		version of angular that the tutorial uses and the 
+		one it loads, are different. 
+
+
+		My next problem is a bit weird. Currently, 
+		the angular is functioning as a REST app. 
+		The service "konStores" retrieves json that 
+		holds all the data on the stores; address, names,
+		phone numbers, you get the idea. It stores 
+		it in a variable called storeId.
+
+		From there, the controller "locdetailCtrl" uses that service
+		to retrieve the data associated with that storeId. The view "store-details.html", 
+		uses that controller to find that data from the url params to display that info. 
+
+		The data sends but only has a whole json object... So using
+
+				{{store.name}} 
+
+		Won't show anything.... BUT!
+
+				{{store}}
+
+
+
+8/7 - 
+
+		So searching on reddit's /r/angularjs and talking 
+		to Chris, brough up an interesting point. Since 
+		the file is still loading and it's coming in as 
+		an array. I can simply put a param in there and 
+		search for the index. 
+
+		Now my main problem is that the app doesn't 
+		seem to read the params from the link. 
+
+		So if I do {{store[ENTER URL PARAMS]}} 
+		I should get the desired object. Nothing 
+		comes out though, so something isn't reading 
+		the params. 
+
+		{{store[0]}} works though. 
+
+
+8/6 part 2 - electric boogaloo
+
+		Chris, along with some people over on 
+		reddit's /r/angularjs brought up a good 
+		point to me. 
+
+		The json is passing as an array, so I can 
+		still access the objects inside of it through
+		an index. Now I have to make a filter that 
+		checks for that id tag and returns the proper 
+		object within the json.
+
+		What's weird is that I followed the same format 
+		as the official angular tutorial. 
+		I should re check my steps. 
+
+
+
+8/9 - 
+
+		
+		Sup.
+
+		...
+
+		How you been? 
+
+		...
+
+		Awesome. 
+
+		...I gotta stop playing Earthbound.
+
+		Alright, so new/same stuff. It seems that at first, the $scope.
+		whichScope kept coming up undefined. I've added the whichStore
+		outside of the http.get to make it a bit more of a global 
+		variable FOR THAT FUNCTION. 
+
+		It does output the proper array index, oh yeah more 
+		about that later, into the console. So, I'm not 
+		sure what's the problem. It's probably the way I'm 
+		calling it into the {{ }} (<-- completely forgot what
+		 that was called again).  
+
+		About the array index, since the json file was being called,
+		 and I was able to reference any object within that json array 
+		through the index; I've put a .indexOf() to catch that 
+		index int. That's part of the whole $scope.whichStore var. 
+
+
+8/10 - 
+
+		Alright! Awesome! 
+
+		I fixed the routing issue. 
+
+		Making the filter, I was able to now use the id tag in
+		the json file to call the info that I need. The way I did
+		it was by making a scope variable called storeId in the 
+		locdetail.js file. 
+
+		In this storeId, it gets the value of the $routeParams.storeId.
+		Back in the loc.html, you can see that the a href tag, that 
+		there is an expression (It's these things {{ }} ). When the
+		$http.get activates, it searches the directory for the json file;
+		when successful, it filters the file. The "entry" parameter
+		is an empty variable for the json array objects. The filter
+		won't stop until it finds the object containing the EXACT 
+		(indicated with the triple equal signs === ). Once it does,
+		it returns as an array of objects except that the found object
+		will be the first item on the index or index[0]. That's what 
+		the [0] at the end of it is for. 
+
+		That all carries over to the store-details.html, which now 
+		needs to be styled. 
+
+		Btw, that cat has been giving me the badass look the whole time.
+
+
+8/14 -
+
+		Wow, okay, where to start.
+
+		First, hola. It's been a while. 
+
+		Been busy with work and working on this,
+		and I've put a lot of work on this.
+		Instead of my usual winging it, I've made
+		mockups on GIMP, which is surprisingly 
+		easier to use than photoshop...
+
+		From there, I made mockups of the main
+		html, and the locations.
+
+		The locations menu, I took inspiration
+		from GTA5, on their multiplayer
+		mission vote menu. 
+
+		Most of the desktop version is done,
+		still gotta fix up the location profile
+		page, add images, and work with the
+		instagram API which I've still haven't
+		checked out... 
+
+9/2 - 
+
+		So I put in more work, the menu came out
+		looking good. Still needs some work but
+		all the items that I needed to put in
+		is in there. 
+
+		Now I gotta work on the store page,
+		along with getting new pictures and 
+		using the instagram api. 
+
+
+
+11/16 - 
+
+		Annnd now this gets restarted. 
+
+		Got hired as their web dev, going to develop this
+		the best I can. 
+
+		Luckily all it needs a basic style change, and make it mobile.
+
+		I was able to salvage a lot of stuff so it didn't hinder my timeline for this. 
+
+		Changed the whole layout
+		Added more responsive classes (Must keep watch on bootstrap updates.)
+		Added mobile version of VIEWS/MAIN.HTML (With responsive classes)
+
+		Have to do:
+		Must fix navbar. (It's not being responsive.)
+		Do menu. (Up to debate with siteowners.)
+		Redo the whole store locations and details menu. (With responsive.)
+
+
+11/20 - 
+
+
+		
+		So much work...
+
+		Made it mobile on certain pages. 
+
+		Right now it's colorless/neutral colors (Colours for you foriegners).
+
+		So what's my current objectives? 
+
+		- Update the items database with the description info (...Fuck)
+
+		- Get the color scheme from Per/Ronnie/I don't know who else.
+
+		- Fix the locations page (Using a modified version of the display that I 
+									have for the Menu and What's Brewin' content. )
---
+++ client/.tmp/concat/scripts/scripts.js
@@ -0,0 +1,333 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name tempGitApp
+ * @description
+ * # tempGitApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('tempGitApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'firebase'
+  ])
+  .config(["$routeProvider", "$locationProvider", function ($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(false);
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'UpdateCtrl'
+      })
+      .when('/menu', {
+        templateUrl: 'views/menu.html',
+        controller: 'konmenuCtrl'
+      })
+      .when('/locs',{
+        templateUrl:'views/locs.html',
+        controller: 'LocateCtrl'
+      }).
+      when('/locs/:storeId', {
+        templateUrl:'views/store-detail.html',
+        controller: 'LocdetailCtrl'
+      })
+      .when('/admin',{
+        templateUrl: 'views/admin.html',
+        controller: 'UpdateCtrl'
+      })
+      .when('/media', {
+        templateUrl: 'views/media.html',
+        controller: 'MediaCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
+
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tempGitApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the tempGitApp
+ */
+angular.module('tempGitApp')
+  .controller('MainCtrl', ["$scope", function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+    $scope.locations = [
+    	'5th avenue',
+    	'7th avenue'
+    ];
+  }]);
+
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tempGitApp.controller:LocateCtrl
+ * @description
+ * # LocateCtrl
+ * Controller of the tempGitApp
+ */
+angular.module('tempGitApp')
+  .controller('LocateCtrl', ["$scope", "$routeParams", "$http", "konStores", function ($scope, $routeParams, $http, konStores) {
+   		$scope.stores = konStores.query({storeId: $routeParams.storeId});
+
+
+
+
+   		/*$http.get('stores/stores.json').success (function(data){
+   			$scope.storeVar = data; 
+   		});
+									Back up just incase the service doesn't work. 	
+
+									*/   		
+  }]);
+
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name tempGitApp.konStores
+ * @description
+ * # konStores
+ * Service in the tempGitApp.
+ */
+angular.module('tempGitApp')
+  .service('konStores', ["$resource", function($resource) {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  	return $resource('infos/stores.json',{},{
+  		query:{method:'GET',params:{storeId:'stores'}, isArray:true}
+  	});
+  }]);
+
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name tempGitApp.directive:lights
+ * @description
+ * # lights
+ */
+angular.module('tempGitApp')
+  .directive('lights', function () {
+    return {
+      template: '<div></div>',
+      restrict: 'E',
+      link: function postLink(scope, element, attrs) {
+        element.text('this is the lights directive');
+      }
+    };
+  });
+
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tempGitApp.controller:LocdetailCtrl
+ * @description
+ * # LocdetailCtrl
+ * Controller of the tempGitApp
+ */
+angular.module('tempGitApp') // ...Change this soon.
+  .controller('LocdetailCtrl', ["$scope", "$http", "$routeParams", function ($scope, $http, $routeParams) {
+    /*	$scope.storeId = konStores.query({storeId: $routeParams.storeId});
+
+    	$scope.store = konStores.filter(function(entry){
+
+    		return entry.id === $scope.storeId
+    	})[0];
+
+
+        console.log($scope.storeId);
+*/
+
+    $scope.storeId = $routeParams.storeId;
+
+    $http.get('infos/stores.json').success(function(data){
+    	$scope.store = data.filter(function(entry){ 
+
+    		return entry.id === $scope.storeId;
+    	})[0];
+    });
+    
+  }]);
+
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tempGitApp.controller:KonmenuCtrl
+ * @description
+ * # KonmenuCtrl
+ * Controller of the tempGitApp
+ */
+angular.module('tempGitApp')
+  .controller('konmenuCtrl', ["$scope", "$http", function ($scope,$http) {
+    	$http.get('infos/menu.json').success(function(data){
+    		$scope.menuItems = data;
+    	});
+  }]);
+
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tempGitApp.controller:UpdatectrlCtrl
+ * @description
+ * # UpdatectrlCtrl
+ * Controller of the tempGitApp
+ */
+angular.module('tempGitApp')
+  .controller('UpdateCtrl', ["$scope", "$firebaseObject", "$firebaseArray", function ($scope, $firebaseObject, $firebaseArray) {
+
+ 
+  			/*var ref = new Firebase('https://konditori.firebaseIO.com/Updates');
+  			var syncObject = $firebaseArray(ref);
+  		
+  			syncObject.$bindTo($scope, "data");*/
+
+  			
+
+  				var ref = new Firebase('https://konditori.firebaseIO.com/');
+
+  				$scope.Updates = $firebaseArray(ref);
+
+
+
+  				$scope.addUpdate = function(){
+  						    var re = /\/n/g; 
+   							 var str = $scope.update;
+     						var subst = '<br>';
+    					var result = str.replace(re, subst);
+  					
+  						var title = $scope.title;
+  						$scope.Updates.$add({
+  							Title:title,
+  							Body: result
+  						});
+
+  						$scope.update = '';
+  				}
+
+  				
+
+  			
+  }]);
+
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tempGitApp.controller:MediaCtrl
+ * @description
+ * # MediaCtrl
+ * Controller of the tempGitApp
+ */
+angular.module('tempGitApp')
+  .controller('MediaCtrl', ["$scope", function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  }]);
+
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name tempGitApp.directive:navMenu
+ * @description
+ * # navMenu
+ */
+angular.module('tempGitApp')
+  .directive('navMenu', function () {
+    return {
+      replace:true,
+      templateUrl: 'templates/navMenu.html',
+      restrict: 'EA',
+      link: function postLink(scope, element, attrs) {
+        var navBarz = $('.navBigStyle a');
+        
+        var backImage = $(this).css('background-image','url("/images/vinSwedFlag.jpg');
+
+        navBarz.mouseenter(function(){
+
+        	
+        	$(this).fadeIn(1500).addClass('navMenuActive').css('color','white');
+
+        });
+        navBarz.mouseleave(function() {
+        	$(this).removeClass('navMenuActive').css('color','#777');
+        });
+        navBarz.click(function(){
+        	$(this).removeClass('navMenuActive')
+        	$(this).addClass('navMenuActive')
+        })
+      }
+    };
+  });
+
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name tempGitApp.directive:emailContact
+ * @description
+ * # emailContact
+ */
+angular.module('tempGitApp')
+  .directive('emailContact', function () {
+    return {
+      replace:true,
+      templateUrl: 'templates/emailContact.html',
+      restrict: 'EA',
+      link: function postLink(scope, element, attrs) {
+       scope.completedEmail= {};
+
+       scope.updates = function(user) {
+       	scope.completedEmail = angular.copy(user);
+       	console.log(scope.completedEmail);
+       	
+       };
+
+       scope.reset = function() {
+       	scope.completedEmail = angular.copy(scope.completedEmail);
+       };
+       scope.reset();
+      }
+    };
+  });
+
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name tempGitApp.directive:twitterDisplay
+ * @description
+ * # twitterDisplay
+ */
+angular.module('tempGitApp')
+  .directive('twitterDisplay', function () {
+    return {
+      replace:true,		
+      templateUrl: 'templates/twitterDisplay.html',
+      restrict: 'EA',
+      link: function postLink(scope, element, attrs) {
+        
+      }
+    };
+  });
